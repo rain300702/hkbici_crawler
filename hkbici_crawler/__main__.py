0b43f2316519c37f9f99723fe20ea4a6e8587d8e
WJQ <rain300702@hotmail.com>
2015-12-22 02:33:33 +0800

    Crawler for hkbici.com :D

---
+++ hkbici_crawler/__main__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from crawler import Crawler
+from cmdParser import parser
+
+
+def main():
+    try:
+        args = parser.parse_args()
+        crawler = Crawler(args)
+        crawler.start()
+    except KeyboardInterrupt:
+        print 'Process aborted by user.'
+
+if __name__ == '__main__':
+    main()
